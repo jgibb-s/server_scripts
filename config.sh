294e077ab82097af45b826eea128e4af4229c0c3
jgibb-s <joshbtgibbs@gmail.com>
2024-10-07 09:40:59 -0700

    Updated variables in some scripts

--- config.sh
+++ config.sh
@@ -6,11 +6,11 @@ sudo="sudo"
 # discspace
 maxDiscUsage=80
 barWidth=65
-mountpoints="/home/josh /mnt/disk1 /mnt/disk2 /mnt/disk3 /mnt/parity /mnt/storage"
+mountpoints="${HOME} /mnt/disk1 /mnt/disk2 /mnt/disk3 /mnt/parity /mnt/storage"
 
 # hddtemp
 my_drives="sda sdb sdc sdd sdf"
 my_target_temp=38
 
 # ssl
-ssl_domains="example.com domain.com"
+#ssl_domains="example.com domain.com"
